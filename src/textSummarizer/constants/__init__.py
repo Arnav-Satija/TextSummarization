1fd0df4666c8a3c0a38bc50fec593d00ff4a1d1e
Arnav Satija <arnav0114.be20@chitkara.edu.in>
2024-01-17 13:56:08 +0530

    data ingestion

---
+++ src/textSummarizer/constants/__init__.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path(r"D:/Personal_pro/ML/TextSummarization/config/config.yaml")
+PARAMS_FILE_PATH = Path(r"D:/Personal_pro/ML/TextSummarization/params.yaml")
